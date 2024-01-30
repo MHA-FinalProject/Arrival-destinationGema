09035d1dd198f8e0c8f08a96df8f9a11a166a8c8
Hod_wys <92233601+hodwys@users.noreply.github.com>
2024-01-30 14:29:50 +0200

    Add files via upload

---
+++ Assets/GoToNextLevel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoNextLevel : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = Vector3.zero;
+            SceneManager.LoadScene(sceneName);    // Input can either be a serial number or a name; here we use name.
+        }
+    }
+}
